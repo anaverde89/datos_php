b7f23588b1a648aae37e93ec5bfced06bfa54402
Ana Verde <anaverde89@gmail.com>
2021-05-23 11:44:29 -0400

    Extraccion de datos

---
+++ extraccion/index.php
@@ -0,0 +1,21 @@
+<?php
+/* $data = 'Estudio PHP';
+echo $data{0}; */
+
+/*$post = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sint quisquam doloribus sapiente, vero voluptatum esse deleniti eaque assumenda eligendi harum obcaecati sit fuga ad. Labore veritatis dolore aspernatur nesciunt!';
+$extract = substr($post, 0, 20);
+
+echo "$extract... [ver más]";*/
+
+/* $data = 'javascript, php, laravel';
+$tags = explode(', ', $data);
+echo '<pre>';
+var_dump($tags); */
+
+/* $courses = ['javascript', 'php', 'laravel'];
+echo implode(', ', $courses); */
+
+$course = '    Curso de PHP     ';
+$course = trim($course);
+echo '<pre>';
+echo "Estoy en un $course, otro texto";
