51de49f2da7902a3c8461282f9473139357d6257
Ana Verde <anaverde89@gmail.com>
2021-05-25 15:17:51 -0400

    Revision de proyecto

--- proyecto/tests/ValidateTest.php
+++ proyecto/tests/ValidateTest.php
@@ -4,12 +4,30 @@ use PHPUnit\Framework\TestCase;
 use App\Validate;
 
 class ValidateTest extends TestCase{
-    public function test_email()
+    public function testEmail()
     {
         $email=Validate::email('a@ex.com');
         $this->assertTrue($email);
 
         $email=Validate::email('a@@ex.com');
         $this->assertFalse($email);
+    }
+
+    public function testUrl()
+    {
+        $url=Validate::url('https://platzi.com');
+        $this->assertTrue($url);
+
+        $url=Validate::url('platzi.com');
+        $this->assertFalse($url);
+    }
+
+    public function testPassword()
+    {
+        $password=Validate::password('123456aZ@');
+        $this->assertTrue($password);
+
+        $password=Validate::password('123456aA*');
+        $this->assertFalse($password);
     }
 }
