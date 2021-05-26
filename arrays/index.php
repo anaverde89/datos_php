a3b85c34219262efa2b03d3157ac9c6e39a28b12
Ana Verde <anaverde89@gmail.com>
2021-05-26 11:15:08 -0400

    Array complejo

--- arrays/index.php
+++ arrays/index.php
@@ -1,10 +1,18 @@
 <?php
 
 $courses=[
-    'javascript',
-    'laravel',
-    'php',
-    'vuejs'
+    'frontend'=>'javascript',
+    'framework'=>'laravel',
+    'backend'=>'php',
 ];
-//echo '<pre>';
-var_dump($courses);
+
+/*foreach ($courses as $course) {
+    echo "$course<br>";
+}*/
+
+function upper( $course,$key)
+{
+    echo $key . ': ' . strtoupper($course).'<br>';
+}
+
+array_walk($courses, 'upper');
